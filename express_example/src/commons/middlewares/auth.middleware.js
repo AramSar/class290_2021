711151d5fe47d115e403d64294c945234d3adc47
aram_sargsyan <aram.sargsyan@betconstruct.com>
2021-04-01 02:11:55 +0400

    blocking authorized actions of locked users

--- express_example/src/commons/middlewares/auth.middleware.js
+++ express_example/src/commons/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-const { Forbidden } = require('http-errors');
+const { Forbidden, Locked } = require('http-errors');
 const { validateToken } = require('../../auth/auth.service');
 const users = require('../../users/users.service');
 const { isPublicPath } = require('../util');
@@ -11,6 +11,9 @@ const jwtMiddleware = async (req, res, next) => {
             token = req.header('Authorization').split(' ')[1];
             const user = validateToken(token);
             const dbUser = await users.findOne(user.userId);
+            if(dbUser.isLocked){
+                next(new Locked('The user is locked!'))
+            }
             user.role = dbUser.role;
             req.user = user;
         } catch (err) {
